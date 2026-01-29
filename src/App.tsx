85743fd4a7c83d8c63048e93dd14a3192de50169
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-01-29 17:13:32 -0300

    feat: add stores to App

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+// import { useBoundStore } from "./shared/store/slice.store";
+// import { useZooStore } from "./shared/store/zoo.store";
+// import { useBearStore } from "./shared/store/bear.store";
+// import { useCountStore } from "./shared/store/count.store";
+
+export const App = () => {
+  // const { count, increment, decrement, reset } = useCountStore();
+  // const { bears, feed } = useBearStore();
+  // const { bears, increase, reset } = useZooStore();
+  // const { login, logout, token, updateProfile, user } = useBoundStore();
+
+  return <></>;
+};
